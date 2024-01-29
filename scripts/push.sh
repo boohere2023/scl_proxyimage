7f59c1eb9a4964fb88312535b169e42400c2494c
boohere2023 <143770773+boohere2023@users.noreply.github.com>
2024-01-29 09:45:38 +0800

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 #Commit Image
 docker commit $(docker container ls  | grep 'aapanel' | awk '{print $1}') sclbo/`hostname`:`date +%Y%m%d`
+Docker tag sclbo/`hostname`:`date +%Y%m%d` sclbo/`hostname`:latest
 #Push image to DockerHUB
-docker push sclbo/`hostname`:`date +%Y%m%d`
+docker push sclbo/`hostname` -a
 #Delete Image  after push
 docker image rm sclbo/`hostname`:`date +%Y%m%d`
+
