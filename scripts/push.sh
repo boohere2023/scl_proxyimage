9cc4aeb01c3b943702173c568a3597e1e0f39608
boohere2023 <143770773+boohere2023@users.noreply.github.com>
2024-03-12 13:03:22 +0800

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 #Commit Image
 docker commit $(docker container ls  | grep 'aapanel' | awk '{print $1}') sclbo/`hostname`:`date +%Y%m%d`
-Docker tag sclbo/`hostname`:`date +%Y%m%d` sclbo/`hostname`:latest
+#docker tag sclbo/`hostname`:`date +%Y%m%d` sclbo/`hostname`:latest
 #Push image to DockerHUB
-docker push sclbo/`hostname` -a
+docker push sclbo/`hostname`:`date +%Y%m%d`
 #Delete Image  after push
 docker image rm sclbo/`hostname`:`date +%Y%m%d`
 #Send noti to Telegram
